41152392b5d18b353f13704ad28fd829065a5358
berkekendirli <efekndrl@gmail.com>
2025-08-26 16:53:11 +0300

    custom button component

--- app/src/main/java/com/bek/waterreminder/ui/components/CustomButton.kt
+++ app/src/main/java/com/bek/waterreminder/ui/components/CustomButton.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
@@ -28,6 +29,7 @@ fun CustomButton(
   Box(
       modifier =
           modifier
+              .clip(RoundedCornerShape(8.dp))
               .clickable { onClick() }
               .border(
                   width = 1.dp,
