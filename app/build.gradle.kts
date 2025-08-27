14d7e471e059faeb82ea3de7edd6cdd0b8990a09
berkekendirli <efekndrl@gmail.com>
2025-08-27 16:43:20 +0300

    datastore - coroutines packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,9 @@ android {
 
 dependencies {
   implementation("androidx.navigation:navigation-compose:2.9.3")
+  implementation("androidx.datastore:datastore-preferences:1.1.7")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime.ktx)
